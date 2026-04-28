1d38009fec1d5bcdb77803d6b814b7264c0c0bd7
naina <nainamadan567@gmail.com>
2026-04-28 15:27:38 +0530

    new changes

--- server/index.js
+++ server/index.js
@@ -20,6 +20,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:5175",
+  "https://ai-interview-platform-8f4n.vercel.app"
 ];
 
 app.use(cors({
